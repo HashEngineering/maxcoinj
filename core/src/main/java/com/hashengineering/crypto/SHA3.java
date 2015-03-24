cbef3b8402089ced9b3fcbc86fee3ac289980a36
HashEngineering <hashengineeringsolutions@gmail.com>
2015-03-23 22:28:54 -0700

    Added SHA3 hash code

--- core/src/main/java/com/hashengineering/crypto/SHA3.java
+++ core/src/main/java/com/hashengineering/crypto/SHA3.java
@@ -1,6 +1,6 @@
 package com.hashengineering.crypto;
 
-import fr.cryptohash.*;
+import fr.cryptohash.Keccak256;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +15,7 @@ public class SHA3 {
     static {
 
         try {
-            System.loadLibrary("x11");
+            System.loadLibrary("keccak");
             native_library_loaded = true;
         }
         catch(UnsatisfiedLinkError x)
@@ -28,20 +28,11 @@ public class SHA3 {
         }
     }
 
-    public static byte[] x11Digest(byte[] input, int offset, int length)
+    public static byte[] digest(byte[] input, int offset, int length)
     {
-        byte [] buf = new byte[length];
-        for(int i = 0; i < length; ++i)
-        {
-            buf[i] = input[offset + i];
-        }
-        return x11Digest(buf);
-    }
-
-    public static byte[] x11Digest(byte[] input) {
         //long start = System.currentTimeMillis();
         try {
-            return native_library_loaded ? x11_native(input) : x11(input);
+            return native_library_loaded ? keccak256_native(input, offset, length) : keccak256(input, offset, length);
         } catch (Exception e) {
             return null;
         }
@@ -51,49 +42,33 @@ public class SHA3 {
         }
     }
 
-    static native byte [] x11_native(byte [] input);
+    public static byte[] digest(byte[] input) {
+        //long start = System.currentTimeMillis();
+        try {
+            return native_library_loaded ? keccak256_native(input, 0, input.length) : keccak256(input);
+        } catch (Exception e) {
+            return null;
+        }
+        finally {
+            //long time = System.currentTimeMillis()-start;
+            //log.info("X11 Hash time: {} ms per block", time);
+        }
+    }
+
+    static native byte [] keccak256_native(byte [] input, int offset, int length);
 
 
-    static byte [] x11(byte header[])
+    static byte [] keccak256(byte input[])
     {
-        //Initialize
-        Sha512Hash[] hash = new Sha512Hash[11];
 
-        //Run the chain of algorithms
-        BLAKE512 blake512 = new BLAKE512();
-        hash[0] = new Sha512Hash(blake512.digest(header));
+        Keccak256 keccak = new Keccak256();
+        return keccak.digest(input);
+    }
 
-        BMW512 bmw = new BMW512();
-        hash[1] = new Sha512Hash(bmw.digest(hash[0].getBytes()));
-
-        Groestl512 groestl = new Groestl512();
-        hash[2] = new Sha512Hash(groestl.digest(hash[1].getBytes()));
-
-        Skein512 skein = new Skein512();
-        hash[3] = new Sha512Hash(skein.digest(hash[2].getBytes()));
-
-        JH512 jh = new JH512();
-        hash[4] = new Sha512Hash(jh.digest(hash[3].getBytes()));
-
-        Keccak512 keccak = new Keccak512();
-        hash[5] = new Sha512Hash(keccak.digest(hash[4].getBytes()));
-
-
-        Luffa512 luffa = new Luffa512();
-        hash[6] = new Sha512Hash(luffa.digest(hash[5].getBytes()));
-
-        CubeHash512 cubehash = new CubeHash512();
-        hash[7] = new Sha512Hash(cubehash.digest(hash[6].getBytes()));
-
-        SHAvite512 shavite = new SHAvite512();
-        hash[8] = new Sha512Hash(shavite.digest(hash[7].getBytes()));
-
-        SIMD512 simd = new SIMD512();
-        hash[9] = new Sha512Hash(simd.digest(hash[8].getBytes()));
-
-        ECHO512 echo = new ECHO512();
-        hash[10] = new Sha512Hash(echo.digest(hash[9].getBytes()));
-
-        return hash[10].trim256().getBytes();
+    static byte [] keccak256(byte [] input, int offset, int length)
+    {
+        Keccak256 keccak256 = new Keccak256();
+        keccak256.update(input, offset, length);
+        return keccak256.digest();
     }
 }
